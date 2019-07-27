1f35a130d6a17674787365051f010af813bd5cac
Author Name <13253042+johanbook@users.noreply.github.com>
2019-07-27 08:30:15 +0200

    Update setup-linux-python-server.sh

--- setup-linux-python-server.sh
+++ setup-linux-python-server.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 # Configure an Ubuntu Python server
+# run using <host name><admin password>
 
 # Install security updates
 apt update -y && apt upgrade -y
 
 # Set host name
-hostnamectl set-hostname <host-name>
+hostnamectl set-hostname $1
 # set hostname in /etc/hosts
 
 # Create limited user account
-adduser admin # also password
+adduser admin -p $2
 adduser admin sudo
 
 # Setup ssh
