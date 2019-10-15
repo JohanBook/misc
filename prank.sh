76dc419e3ae402a99a0f4c2906a25044c3b925bd
Author Name <13253042+johanbook@users.noreply.github.com>
2019-10-15 08:33:29 +0200

    Update prank.sh

--- prank.sh
+++ prank.sh
@@ -5,4 +5,4 @@ tee -a 'watch -n $RANDOM echo "File could not be found" &>/dev/null &' | ~/.zshr
 
 # Randomly pick a background
 wget -o ~/Pictures/duckling.jpg https://wallpaperplay.com/walls/full/b/3/5/256436.jpg
-echo 'if (( RANDOM % 9 )); then feh -bg-scale ~/Pictures/duckling.jpg;' >> ~/.i3/config                                                                            
+echo 'exec if (( RANDOM % 9 )); then feh -bg-scale ~/Pictures/duckling.jpg;' >> ~/.i3/config                                                                            
