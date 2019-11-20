c2620cec436ce139410c8f05877745f3e3314184
Author Name <13253042+johanbook@users.noreply.github.com>
2019-11-20 10:13:26 +0100

    Create server.js

---
+++ expressjs/server.js
@@ -0,0 +1,21 @@
+const express = require("express");                                                                                                                                           
+const logger = require("morgan");
+const request = require("request");
+const app = express();
+
+app.use(logger("combined"));
+
+app.use(express.static("build"));
+
+/* Forward api requests to api */
+app.route("/*").all(function(req, res) {
+  const url = "http://api:5000" + req.url;
+  req.pipe(request(url)).pipe(res);
+});
+
+/* Serve web page */
+app.get("/", function(req, res) {
+  res.sendFile("index.html");
+});
+
+app.listen(process.env.PORT || 8080);
