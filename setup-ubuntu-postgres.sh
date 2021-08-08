86721a7b3e9cd61875a149f27be8374a191a9080
Author Name <13253042+johanbook@users.noreply.github.com>
2021-08-08 10:13:55 +0200

    refactor: remove redundant files

--- setup-ubuntu-postgres.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Configure an Ubuntu Python server
-# run using <host name><admin password>
-
-# Install security updates
-apt update -y && apt upgrade -y
-
-# Install postgress
-apt install -qy postgresql postgresql-contrib
